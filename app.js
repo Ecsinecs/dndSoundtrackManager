a513d49c4fe6063cedb8c655aa1174d9d90230b7
Lucas Jähnel <jahnelemanuel@gmail.com>
2024-06-12 22:48:27 -0300

    things done :D
    button for playing song is functional, now working in fadeIn when playing a song

--- app.js
+++ app.js
@@ -1,36 +1,63 @@
 class Song {
-    constructor(name, audio, image) {
-        this.name = name;
-        this.audio = audio;
-        this.image = image;
-        this.id = id;
-    }
+  constructor(name, audio, image) {
+    this.name = name;
+    this.audio = audio;
+    this.image = image;
+    this.id = id;
+  }
 }
 
 const songs = [];
 
 //Obtener canciones
-const getSongs = async ()=>{
-    const response = await fetch("./songs.json")
-    const data = await response.json();
-    data.forEach(element => {
-        songs.push(new Song(element.name, element.audio, element.image, id = "" + (songs.length + 1)));
-    })
-    console.log(songs);
-    showDOM()
-}
+const getSongs = async () => {
+  const response = await fetch("./songs.json");
+  const data = await response.json();
+  data.forEach((element) => {
+    songs.push(
+      new Song(
+        element.name,
+        element.audio,
+        element.image,
+        (id = "" + (songs.length + 1))
+      )
+    );
+  });
+  console.log(songs);
+  showDOM();
+};
 
 function showDOM() {
-    for (const song of songs)  {
-        let div= document.createElement("div")
-        div.classname = "songCard"
-        div.innerHTML =
-        `
-        <p class="songName" style="background-image: url('${song.image}')">${song.name}</p>
-            <source src="${song.audio}">
-        `
-        container.appendChild(div)
-    }
+  for (const song of songs) {
+    let div = document.createElement("div");
+    div.classname = "songCard";
+    div.innerHTML = `
+        <button id="btn${song.name}" class="songName" style="background-image: url('${song.image}')">${song.name}</button>
+        `;
+    container.appendChild(div);
+
+    const btn = document.getElementById(`btn${song.name}`);
+
+    btn.addEventListener("click", () => {
+      createSong(song);
+    });
+  }
 }
 
-getSongs()
+function createSong(song) {
+  let div = document.createElement("div");
+  div.classname = "audioControlls";
+  div.id = `audio${song.name}`;
+  div.innerHTML = `
+    <audio src="${song.audio}" autoplay controls loop class="audioControls" id="audioController${song.name}">`;
+
+  audios.appendChild(div);
+
+  const actualSong = document.getElementById(`audioController${song.name}`);
+  actualSong.volume = 0
+  $audio.animate({volume: 1.0}, 1000)
+}
+
+function delPrevSong() {}
+
+getSongs();
