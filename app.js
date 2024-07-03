a49501564d3ff86b8de971709575ea38809c9ef6
Lucas Jähnel <jahnelemanuel@gmail.com>
2024-07-03 00:12:39 -0300

    New functionalities and songs added

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 class Song {
-  constructor(name, audio, image, fadeInTimer, id, category, playing) {
+  constructor(
+    name,
+    audio,
+    image,
+    fadeInTimer,
+    id,
+    category,
+    playing,
+    randomStart,
+    loops
+  ) {
     this.name = name;
     this.audio = audio;
     this.image = image;
@@ -7,12 +17,14 @@ class Song {
     this.fadeInTimer = fadeInTimer;
     this.category = category;
     this.playing = false;
+    this.randomStart = randomStart;
+    this.loops = loops;
   }
 }
 
 const songs = [];
 
-//Obtener canciones
+//Gets songs from JSON
 const getSongs = async () => {
   const response = await fetch("./songs.json");
   const data = await response.json();
@@ -25,13 +37,17 @@ const getSongs = async () => {
         element.fadeInTimer,
         (id = "" + (songs.length + 1)),
         element.category,
-        element.playing
+        element.playing,
+        element.randomStart,
+        element.loops
       )
     );
   });
   console.log(songs);
   showDOM();
 };
+/* Base app has spaces for name, audio, image, fade in timer, an id, category, a boolean for checking if song is playing, 
+a boolean for random start or if it starts from the beginning, and a boolean for looping comprobation. More can be added. Id doesn't get a use.*/
 
 //Shows all songs avalaible
 function showDOM() {
@@ -42,21 +58,28 @@ function showDOM() {
         <button id="btn${song.name}" class="songIcon" style="background-image: url('${song.image}')"></button>
         <p class="songName">${song.name}</p>
         `;
-    if (song.category == "Combat") {
-      containerCombat.appendChild(div);
-    } else if (song.category == "Travel") {
-      containerTravel.appendChild(div);
-    }
 
+    //Append to respective container by category
+    const container = document.getElementById(`container${song.category}`);
+    container.appendChild(div);
+
+    //Generates the audio controlls
     let div2 = document.createElement("div");
     div2.classname = "audioControlls";
     div2.id = `audio${song.name} generalID`;
-    div2.innerHTML = `
+    //Checks if song loops
+    if (song.loops === true) {
+      div2.innerHTML = `
         <audio src="${song.audio}" controls loop class="audioControls" id="audioController${song.name}">`;
+    } else {
+      div2.innerHTML = `
+        <audio src="${song.audio}" controls class="audioControls" id="audioController${song.name}">`;
+    }
     audios.appendChild(div2);
 
     const btn = document.getElementById(`btn${song.name}`);
 
+    //Plays or pauses song
     btn.addEventListener("click", () => {
       if (!song.playing) {
         playSong(song);
@@ -67,17 +90,26 @@ function showDOM() {
   }
 }
 
-//Plays song
+//Plays song function
 function playSong(song) {
   const audio = document.getElementById(`audioController${song.name}`);
   stopAllSongs(song.name);
-  audio.currentTime = 0;
   audio.volume = 0;
-  audio.play();
+
+  //Checks if song has random start
+  if (song.randomStart === true) {
+    audio.currentTime = Math.random() * audio.duration;
+    audio.play();
+    console.log(audio.currentTime);
+  } else if (song.randomStart === false) {
+    audio.currentTime = 0;
+    audio.play();
+    console.log(audio.currentTime);
+  }
   fadeIn(song);
 }
 
-//Fade in for song
+//Fade in for song function
 function fadeIn(song) {
   const audio = document.getElementById(`audioController${song.name}`);
   console.log(`Fade in: ${song.name}`);
@@ -95,7 +127,7 @@ function fadeIn(song) {
   }, song.fadeInTimer);
 }
 
-//Fade out and stop for song
+//Fade out and stop for song function
 function fadeOut(song) {
   console.log(`Fade out: ${song.name}`);
   const audio = document.getElementById(`audioController${song.name}`);
@@ -114,22 +146,21 @@ function fadeOut(song) {
   }, 10);
 }
 
-//Stop all songs with a possible exception
+//Stop all songs with a possible exception function
 function stopAllSongs(exception) {
   console.log(`Exception is: ${exception}`);
   for (const song of songs) {
     if (song.name != exception && song.playing === true) {
-      console.log(song.playing)
+      console.log(song.playing);
       fadeOut(song);
     }
   }
 }
 
+//Stop all songs when the title is clicked
 const title = document.getElementById("title");
 title.addEventListener("click", () => {
   stopAllSongs();
-})
-
-
+});
 
 getSongs();
